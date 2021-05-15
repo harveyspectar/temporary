5de583eff550e27a27b1fc989f7566acc2b9add9
P-Salik <peersalik16@gmail.com>
2021-05-15 07:39:28 -0700

    PixelPlusUI

--- build_rom.sh
+++ build_rom.sh
@@ -2,16 +2,23 @@
 
 
 # sync rom
-repo init --depth=1 -u https://github.com/Corvus-ROM/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 -u https://github.com/PixelPlusUI/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
 
 git clone https://github.com/P-Salik/local_manifest --depth=1 -b main .repo/local_manifests
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+# patches
+# boot
+cd external/selinux
+curl -LO https://github.com/PixelExperience/external_selinux/commit/9d6ebe89430ffe0aeeb156f572b2a810f9dc98cc.patch
+patch -p1 < *.patch
+cd ../..
+
 # build
 . build/envsetup.sh
-lunch corvus_RMX1941-userdebug
-make corvus
+lunch aosp_RMX1941-userdebug
+mka bacon -j$(nproc --all)
 
 # upload
 rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P
